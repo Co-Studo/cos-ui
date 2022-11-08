eea2388b700c67a9a2be6e7f8e487267bde35eea
mina-gwak <mina.gwak@kakao.com>
2022-11-09 03:11:52 +0900

    :lipstick: Edit global style

--- packages/playground-react/styles/GlobalStyle.ts
+++ packages/playground-react/styles/GlobalStyle.ts
@@ -3,12 +3,25 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  html {
-    font-family: Roboto, NotoSansKR;
-  }
 
   * {
     box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%; // 10px
+    // 참고: https://github.com/Co-Studo/Co-Studo-front/discussions/53
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.palette.bgColor};
+    color: ${({ theme }) => theme.palette.fontColor};
+    font-size: 1.6rem;
+    font-weight: 400;
+    font-family: 'Noto Sans KR', sans-serif;
+    font-style: normal;
+    line-height: 1.5;
+    letter-spacing: -0.4px;
   }
 
   button {
@@ -18,52 +31,36 @@ const GlobalStyle = createGlobalStyle`
     background-color: inherit;
     border-radius: inherit;
     cursor: pointer;
-    font: inherit;
   }
 
   input {
-    border: none;
-    border-radius: inherit;
     margin: 0;
     padding: 0;
-
-    :focus-visible {
+    border: none;
+    border-radius: inherit;
+    :focus-visible{
       outline: none;
     }
   }
 
-  input[type="file"] {
-    position: absolute;
-    width: 0;
-    height: 0;
-    padding: 0;
-    overflow: hidden;
-    border: 0;
-  }
-
   textarea {
+    max-width: 100%;
+    height: auto;
     margin: 0;
     padding: 0;
     border: none;
-    height: auto;
-    max-width: 100%;
     background-color: inherit;
     border-radius: inherit;
     color: inherit;
-
-    :focus-visible {
-      outline: none;
+    :focus-visible{
+      outline:none;
     }
   }
 
   a {
+    font: inherit;
     color: inherit;
     text-decoration: none;
-  }
-
-  body {
-    background-color: ${({ theme }) => theme.palette.bgColor};
-    color: ${({ theme }) => theme.palette.fontColor};
   }
 `;
 
