68f34ae562a050ac40fddd3aff8e785feef4bae1
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-12-01 15:35:00 +0900

    :wrench: eslint rule 공통으로 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       typescript: {
-        directory: 'packages/primitives',
+        directory: 'packages/playground-react',
       },
     },
   },
@@ -19,7 +19,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.eslint.json',
+    project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
@@ -59,6 +59,9 @@ module.exports = {
     'react/jsx-no-constructed-context-values': 'off',
     'react/no-unknown-property': ['error', { ignore: ['css'] }],
     'react/button-has-type': 'off',
+    'no-await-in-loop': 'off',
+    'no-continue': 'off',
+    'no-restricted-syntax': ['off', 'ForOfStatement'],
     'import/order': [
       'error',
       {
@@ -71,4 +74,36 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['packages/playground-react/**/*.ts?(x)'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: './packages/playground-react/tsconfig.json',
+          },
+        },
+      },
+    },
+    {
+      files: ['packages/primitives/**/*.ts?(x)'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: './packages/primitives/tsconfig.json',
+          },
+        },
+      },
+    },
+    {
+      files: ['packages/react/**/*.ts?(x)'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: './packages/react/tsconfig.json',
+          },
+        },
+      },
+    },
+  ],
 };
