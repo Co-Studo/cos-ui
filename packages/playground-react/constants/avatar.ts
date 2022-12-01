d32924eac3248a98e13df849f7c37bae22f7390f
mina-gwak <mina.gwak@kakao.com>
2022-12-02 01:53:08 +0900

    :lipstick: Edit avatar style and add avatar page

---
+++ packages/playground-react/constants/avatar.ts
@@ -0,0 +1,24 @@
+const AVATARS = [
+  {
+    id: 1,
+    name: 'jamie',
+    src: 'https://avatars.githubusercontent.com/u/62706988?v=4',
+  },
+  {
+    id: 2,
+    name: 'park',
+    src: 'https://avatars.githubusercontent.com/u/58503584?v=4',
+  },
+  {
+    id: 3,
+    name: 'hemdi',
+    src: 'https://avatars.githubusercontent.com/u/34249911?v=44',
+  },
+  {
+    id: 4,
+    name: 'dony',
+    src: 'https://avatars.githubusercontent.com/u/17706346?v=4',
+  },
+];
+
+export default AVATARS;
