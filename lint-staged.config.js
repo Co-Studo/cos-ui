ca99c0e0fc6b2664938813025d5b6c2e41193f1c
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-12-01 18:23:58 +0900

    :wrench: lint-staged type check

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix'],
+  'packages/react/**/*.+(ts|tsx)': () =>
+    'tsc -p packages/react/tsconfig.json --noEmit',
+  'packages/playground-react/**/*.+(ts|tsx)': () =>
+    'tsc -p packages/playground-react/tsconfig.json --noEmit',
+  'packages/primitives/**/*.+(ts|tsx)': () =>
+    'tsc -p packages/primitives/tsconfig.json --noEmit',
 };
