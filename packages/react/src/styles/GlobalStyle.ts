076ca2610b4bc6b055cd5ed5eb8a0cadfdc3e34d
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-12-12 19:10:03 +0900

    :fire: delete default body color

--- packages/react/src/styles/GlobalStyle.ts
+++ packages/react/src/styles/GlobalStyle.ts
@@ -14,7 +14,6 @@ const cosStyle = css`
   }
 
   body {
-    background-color: ${({ theme }) => theme.palette.neutral_1_light};
     color: ${({ theme }) => theme.palette.black};
     font-size: 1.6rem;
     font-weight: 400;
