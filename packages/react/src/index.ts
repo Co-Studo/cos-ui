2ead735b22c052bec71c68d93bb41dd65dccfdec
jindonyy <rsefa94@gmail.com>
2022-12-16 08:17:09 +0900

    :sparkles: add Slider component in react.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -12,5 +12,6 @@ export { BasicTab, BorderTab } from '@components/Tab';
 export { ColumnTable, RowTable, BasicTable } from '@components/Table';
 export { default as Text } from '@components/Text';
 export { default as Icon } from '@components/Icon';
+export { default as Slider } from '@components/Slider';
 export { default as cosStyle, CosStyle } from '@styles/GlobalStyle';
 export type { Theme, Palette } from '@styles/theme';
