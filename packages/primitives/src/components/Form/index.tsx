ea1496ae310f3ee2a0383fab5122c868c65b70f2
Hemudi <ksum1205@naver.com>
2022-12-19 03:25:01 +0900

    :truck: move form field components to Field Directory

--- packages/primitives/src/components/Form/index.tsx
+++ packages/primitives/src/components/Form/index.tsx
@@ -1,7 +1,7 @@
 import Field from '@components/Form/Field';
+import RadioField from '@components/Form/Field/RadioField';
+import TextField from '@components/Form/Field/TextField';
 import FormRoot from '@components/Form/Form';
-import RadioField from '@components/Form/RadioField';
-import TextField from '@components/Form/TextField';
 
 const Form = Object.assign(FormRoot, {
   TextField,
