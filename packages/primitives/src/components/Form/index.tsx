217e6bcff1b8b60831ee384ee952fe50509fff13
Hemudi <ksum1205@naver.com>
2022-12-19 03:43:37 +0900

    :truck: rename Form Field file

--- packages/primitives/src/components/Form/index.tsx
+++ packages/primitives/src/components/Form/index.tsx
@@ -1,11 +1,11 @@
-import Field from '@components/Form/Field';
+import Field from '@components/Form/Field/Field';
 import RadioField from '@components/Form/Field/RadioField';
 import TextField from '@components/Form/Field/TextField';
 import FormRoot from '@components/Form/Form';
 
 const Form = Object.assign(FormRoot, {
-  TextField,
   Field,
+  TextField,
   RadioField,
 });
 
