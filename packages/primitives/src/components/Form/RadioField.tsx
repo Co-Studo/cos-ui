1a2352475c4ff3c51568ea6a453ee4f816549a1a
Hemudi <ksum1205@naver.com>
2022-12-18 17:42:12 +0900

    :sparkles: create Form.RadioField component

---
+++ packages/primitives/src/components/Form/RadioField.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+
+import Field from '@components/Form/Field';
+import Radio from '@components/Radio';
+
+type RadioFieldPros = {
+  label?: ReactNode;
+  name: string;
+  defaultValue?: string;
+  validates?: (<V>(value: V) => boolean)[];
+  options: { value: string; label: ReactNode; disabled?: boolean }[];
+};
+
+const RadioField = ({
+  name,
+  label,
+  validates,
+  options,
+  ...restProps
+}: RadioFieldPros) => (
+  <Field
+    name={name}
+    label={label}
+    validates={validates}
+    render={(subscribe, { value: selectedValue }) => (
+      <Radio selectedValue={selectedValue} {...subscribe}>
+        {options.map(({ value, label: optionLabel, disabled = false }) => (
+          <Radio.Option key={value} value={value} disabled={disabled}>
+            {optionLabel}
+          </Radio.Option>
+        ))}
+      </Radio>
+    )}
+    {...restProps}
+  />
+);
+
+export default RadioField;
