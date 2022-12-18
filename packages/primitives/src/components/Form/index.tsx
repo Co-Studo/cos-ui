b8f987ca5f8cd47ebcf836b5b4c72839fffcd4ce
Hemudi <ksum1205@naver.com>
2022-12-18 17:42:55 +0900

    :lipstick: add form page to playground-react

--- packages/primitives/src/components/Form/index.tsx
+++ packages/primitives/src/components/Form/index.tsx
@@ -1,8 +1,12 @@
+import Field from '@components/Form/Field';
 import FormRoot from '@components/Form/Form';
+import RadioField from '@components/Form/RadioField';
 import TextField from '@components/Form/TextField';
 
 const Form = Object.assign(FormRoot, {
   TextField,
+  Field,
+  RadioField,
 });
 
 export default Form;
