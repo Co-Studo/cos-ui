f6fa77442afddd2f08acd885a518a8aa40356481
jindonyy <rsefa94@gmail.com>
2023-01-09 19:16:14 +0900

    :sparkles: add responsive option to slider page.

--- packages/playground-react/pages/slider/slider.tsx
+++ packages/playground-react/pages/slider/slider.tsx
@@ -3,8 +3,32 @@ import { Button, Slider, Text, FlexBox } from '@cos-ui/react';
 import { myStudies } from '@constants/mock';
 
 const SliderPage = () => (
-  <div css={{ maxWidth: '127rem', margin: '0 auto', padding: '0 4.5rem' }}>
-    <Slider options={{ slidesToShow: 4, slidesMargin: '2rem', speed: 500 }}>
+  <div
+    css={{
+      maxWidth: '127rem',
+      width: '100%',
+      margin: '0 auto',
+      padding: '0 4.5rem',
+    }}
+  >
+    <Slider
+      options={{
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        slidesMargin: '2rem',
+        speed: 500,
+        responsive: [
+          {
+            breakpoint: 820,
+            options: { slidesMargin: '1.5rem', slidesToShow: 3 },
+          },
+          {
+            breakpoint: 420,
+            options: { slidesMargin: '1rem', slidesToShow: 2 },
+          },
+        ],
+      }}
+    >
       <Slider.List>
         {myStudies.map(({ id, title, description }) => (
           <Slider.Item key={id}>
@@ -12,7 +36,9 @@ const SliderPage = () => (
             <Text variant="articleDescription" sx={{ mt: 1 }} ellipsis>
               {description}
             </Text>
-            <FlexBox sx={{ justifyContent: 'space-between', mt: '3rem' }}>
+            <FlexBox
+              sx={{ justifyContent: 'space-between', mt: '3rem', gap: '1rem' }}
+            >
               <Button>체크인</Button>
               <Button variant="outlined">체크아웃</Button>
             </FlexBox>
