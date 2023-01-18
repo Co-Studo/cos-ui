64524bbc3dd575b83eb5fa72b7004e6d26304e33
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-01-18 16:15:23 +0900

    :sparkles: clickable paper

--- packages/react/src/components/Paper/index.tsx
+++ packages/react/src/components/Paper/index.tsx
@@ -1,21 +1,48 @@
-import { ReactNode } from 'react';
-import { CSSObject, useTheme } from 'styled-components';
+import { ReactNode, MouseEvent } from 'react';
+import { useTheme } from 'styled-components';
+
+import { SpacingSX } from '@styles/spacing';
+
+interface PaperSX extends SpacingSX {
+  width?: string;
+  height?: string;
+  borderRadius?: string;
+  boxShadow?: string;
+}
 
 type PaperProps = {
   children: ReactNode;
-  sx?: CSSObject;
+  onClick?: (e: MouseEvent) => void;
+  sx?: PaperSX;
 };
 
-const Paper = ({ children, sx }: PaperProps) => {
+const clickableCss = {
+  cursor: 'pointer',
+  margin: '0.5rem',
+  transition: 'transform 0.1s ease-in-out',
+  ':hover': {
+    transform: 'scale(1.05)',
+  },
+};
+
+const Paper = (props: PaperProps) => {
+  const { children, onClick, sx } = props;
   const theme = useTheme();
+
+  const paperCss = {
+    width: 'max-content',
+    padding: '1rem',
+    borderRadius: '0.5rem',
+    boxShadow: `0 4px 10px ${theme.palette.shadow_100},
+    0 0 4px ${theme.palette.shadow_500}`,
+  };
+
   return (
     <div
+      onClick={onClick}
       css={{
-        width: 'max-content',
-        padding: '1rem',
-        borderRadius: '0.5rem',
-        boxShadow: `0 4px 10px ${theme.palette.shadow_100},
-        0 0 4px ${theme.palette.shadow_500}`,
+        ...paperCss,
+        ...(onClick && clickableCss),
         ...sx,
       }}
     >
