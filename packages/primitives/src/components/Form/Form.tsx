a65522a1e3b8c510186feac4e0287e8d1c554f65
Hemudi <ksum1205@naver.com>
2023-01-24 12:25:35 +0900

    :memo: add @cos-ui/primitives Form document

--- packages/primitives/src/components/Form/Form.tsx
+++ packages/primitives/src/components/Form/Form.tsx
@@ -40,6 +40,37 @@ type FormProps = {
   ) => void;
 };
 
+/**
+ * @name Form
+ * @description 하위 input 컴포넌트들의 입력값과 검증 상태를 관리하는 Form 컴포넌트입니다.
+ * @props validationMode - Input Field 들의 입력값 업데이트와 검증을 어떤 이벤트를 통해 트리거 할지 결정합니다. (onChange | onBlur | onSubmit)
+ * @props onSubmit - form 의 onSubmit 이벤트의 handler 를 설정합니다. 두번째 매개변수인 values 를 통해 하위 Input 필드들의 입력값을 받아 처리 할 수 있습니다.
+ * @example
+ * ```tsx
+ *  <Form onSubmit={handleSubmit} validationMode="onChange">
+    <Form.TextField
+      name="study_name"
+      label="스터디 이름"
+      validates={[checkRequired, checkTitle]}
+      placeholder="8자 이내로 스터디 이름을 입력해주세요."
+    />
+    <Form.RadioField
+      name="personal_information"
+      label="개인정보 수집 동의"
+      validates={[checkAgree]}
+      options={[
+        { value: 'agree', label: '동의' },
+        { value: 'disagree', label: '비동의' },
+      ]}
+    />
+    <FlexBox sx={{ justifyContent: 'center' }}>
+      <Button type="submit" size="medium">
+        제출
+      </Button>
+    </FlexBox>
+  </Form>
+ * ```
+ */
 const Form = ({
   validationMode,
   children,
