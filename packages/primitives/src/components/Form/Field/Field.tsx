838b1034e619bd12f208ac51d276356e495e04ae
Hemudi <ksum1205@naver.com>
2023-01-24 12:29:18 +0900

    :memo: add @cos-ui/primitives Form Fields JSDoc

--- packages/primitives/src/components/Form/Field/Field.tsx
+++ packages/primitives/src/components/Form/Field/Field.tsx
@@ -48,6 +48,30 @@ type FieldProps = {
   ) => ReactNode;
 };
 
+/**
+ * @name Field
+ * @description Form 컴포넌트의 하위 Field를 render props 방식으로 구현할 수 있는 컴포넌트입니다. 반드시 Form 컴포넌트 하위에서 사용해야 합니다.
+ * @props name(required) - 입력 필드의 이름이자 key 값
+ * @props label - 입력 필드의 라벨 값, 단순 텍스트 또는 컴포넌트 입력 가능
+ * @props isShowError - Error 메세지의 출력 여부, default true
+ * @props validates - 입력 필드의 검증 함수 배열, 앞에서부터 순차적으로 실행
+ * @props render - 실질적인 input 컴포넌트를 생성하는 render 함수로 인자로 subscribe, result 를 받습니다.
+ * @example
+ * ```tsx
+    <Form.Field
+    name="study_name"
+    label="스터디 이름"
+    validates={[checkRequired, checkTitle]}
+    render={(subscribe, { error }) => (
+      <TextInput
+        isError={Boolean(error)}
+        placeholder="8자 이내로 스터디 이름을 입력해주세요."
+        {...subscribe}
+      />
+    )}
+  />
+ * ```
+ */
 const Field = ({
   label,
   defaultValue,
--- packages/primitives/src/components/Form/Field/TextField.tsx
+++ packages/primitives/src/components/Form/Field/TextField.tsx
@@ -21,6 +21,33 @@ type TextFieldProps = {
   className?: string;
 };
 
+/**
+ * @name TextField
+ * @description Form 컴포넌트의 하위 Field로 구현된 TextField 컴포넌트입니다. 반드시 Form 컴포넌트 하위에서 사용해야 합니다. multiline type으로 설정 시 TextArea로 사용 가능합니다.
+ * @props name - 입력 필드 이름(required)
+ * @props type - text(default) | email | number | password | search | tel | date | url | multiline
+ * @props resize - none | horizontal | vertical | both
+ * @props label - 입력 필드의 라벨 값, 단순 텍스트 또는 컴포넌트 입력 가능
+ * @props validates - 입력 필드의 검증 함수 배열, 앞에서부터 순차적으로 실행
+ * @example
+ * ```tsx
+ * <Form.TextField
+    type="number"
+    name="max_people"
+    label="최대 인원"
+    defaultValue="3"
+    validates={[checkRequired, checkMinCount, checkMaxCount]}
+    placeholder="스터디 최소 인원은 3명, 최대 인원은 10명입니다."
+  />
+  <Form.TextField
+    type="multiline"
+    name="description"
+    label="스터디 소개"
+    validates={[checkRequired]}
+    placeholder="스터디 소개를 작성해주세요."
+  />
+ * ```
+ */
 const TextField = (props: TextFieldProps) => {
   const {
     name,
