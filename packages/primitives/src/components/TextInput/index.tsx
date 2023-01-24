344687b9ee21681b6f445763cfe62c4b68143812
Hemudi <ksum1205@naver.com>
2023-01-24 12:23:28 +0900

    :memo: add @cos-ui/primitives TextInput document

--- packages/primitives/src/components/TextInput/index.tsx
+++ packages/primitives/src/components/TextInput/index.tsx
@@ -14,16 +14,37 @@ export type TextInputProps = {
   id?: string;
   name?: string;
   type?: TextInputTypes;
-  isError?: boolean;
+  value?: string;
   defaultValue?: string;
   placeholder?: string;
-  required?: boolean;
   min?: string;
   max?: string;
   pattern?: string;
+  required?: boolean;
+  isError?: boolean;
+  isSuccess?: boolean;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
 };
+
+/**
+ *
+ * @name TextInput
+ * @description 1줄의 텍스트를 입력하고 검증 상태를 나타낼 수 있는 컴포넌트입니다.
+ * @props type - text(default) | email | number | password | search | tel | date | url
+ * @props isError - 입력 값 검증 실패
+ * @props isSuccess - 입력 값 검증 성공
+ * @example
+ * ```tsx
+ * <TextInput
+      name="textInputExample"
+      onChange={handleOnChange}
+      defaultValue="default_Input_Value"
+      isError={isError}
+      required
+    />
+ * ```
+ */
 
 const TextInput = ({
   id,
