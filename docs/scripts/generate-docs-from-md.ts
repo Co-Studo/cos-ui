f4d2575fe6526cc632984f965d748af583f237c2
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-03-03 15:45:19 +0900

    feat: generate md with exclude

--- docs/scripts/generate-docs-from-md.ts
+++ docs/scripts/generate-docs-from-md.ts
@@ -7,7 +7,7 @@ import { DOCS_ROOT, PACKAGES_ROOT } from './constants';
 
 const OUT_DIR = DOCS_ROOT;
 
-async function copyMDDocs(outdir: string, exclude: string[] = []) {
+async function copyMDDocs(outdir: string, exclude: string[]) {
   const filepaths = await glob(`**/*.md`, {
     cwd: PACKAGES_ROOT,
     ignore: exclude,
@@ -26,5 +26,11 @@ async function copyMDDocs(outdir: string, exclude: string[] = []) {
 }
 
 export async function generateDocsFromMD() {
-  return Promise.all([copyMDDocs(OUT_DIR)]);
+  return Promise.all([
+    copyMDDocs(OUT_DIR, [
+      '**/README.md',
+      '**/CHANGELOG.md',
+      '**/node_modules/**',
+    ]),
+  ]);
 }
