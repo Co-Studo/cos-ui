42d25c5c0c70836e1266514f09cd8b04162f4080
Hemudi <ksum1205@naver.com>
2023-03-13 13:07:43 +0900

    :heavy_plus_sign: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ['../packages/**/src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
