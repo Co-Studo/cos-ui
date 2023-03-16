32b48f382609233111db4ce422140ad1a3d7ad90
Hemudi <ksum1205@naver.com>
2023-03-17 02:37:41 +0900

    :sparkles: add story code of tab

---
+++ packages/react/src/components/Tab/basicTab.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentMeta } from '@storybook/react';
+
+import { BasicTab } from '@components/Tab';
+
+export default {
+  title: 'React/Tab',
+  component: BasicTab,
+} as ComponentMeta<typeof BasicTab>;
+
+export const Basic = () => (
+  <BasicTab.Group>
+    <BasicTab.List>
+      <BasicTab>Tab A</BasicTab>
+      <BasicTab>Tab B</BasicTab>
+    </BasicTab.List>
+    <BasicTab.Panels>
+      <BasicTab.Panel>Tab Panel 1</BasicTab.Panel>
+      <BasicTab.Panel>Tab Panel 2</BasicTab.Panel>
+    </BasicTab.Panels>
+  </BasicTab.Group>
+);
