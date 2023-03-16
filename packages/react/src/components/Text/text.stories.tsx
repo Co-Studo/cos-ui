7d3ae8a1feb43a3a64ce77686460c151865a3f9e
Hemudi <ksum1205@naver.com>
2023-03-17 02:24:44 +0900

    :sparkles: add playground stories code of text

--- packages/react/src/components/Text/text.stories.tsx
+++ packages/react/src/components/Text/text.stories.tsx
@@ -27,6 +27,35 @@ export default {
   },
 } as ComponentMeta<typeof Text>;
 
-const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+export const Default = () => (
+  <div>
+    <Text variant="sectionTitle" sx={{ mb: 2 }}>
+      Section Title
+    </Text>
+    <Text variant="sectionDescription">Section Description</Text>
+    <Text variant="articleTitle">Article Title</Text>
+    <Text variant="articleDescription">Article Description</Text>
+    <Text
+      variant="sectionDescription"
+      sx={{
+        fontSize: 'large',
+        fontWeight: 'bold',
+        color: 'primary',
+      }}
+    >
+      Section Description
+      <Text.Highlight sx={{ color: 'warning' }}> Highlight</Text.Highlight>
+    </Text>
+    <div css={{ width: '200px' }}>
+      <Text as="p" ellipsis>
+        A falsis, equiso barbatus abaculus.
+      </Text>
+      <Text as="p" ellipsis={2}>
+        Ubi est fidelis galatae? Velox cacula rare perderes bursa est. Heuretess
+        sunt bullas de neuter mensa. Brevis barcas foris desideriums musa est.
+      </Text>
+    </div>
+  </div>
+);
 
-export const Default = Template.bind({});
+export const Custom: ComponentStory<typeof Text> = (args) => <Text {...args} />;
