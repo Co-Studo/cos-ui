a778d3f1ecde9688a6bdcce109326a1aa86cd1e2
Hemudi <ksum1205@naver.com>
2023-03-17 02:01:50 +0900

    :sparkles: add story code of flexbox

---
+++ packages/react/src/components/FlexBox/flexbox.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { useRef } from 'react';
+
+import { FlexBox } from '@components/FlexBox';
+
+export default {
+  title: 'React/FlexBox',
+  component: FlexBox,
+} as ComponentMeta<typeof FlexBox>;
+
+export const Default: ComponentStory<typeof FlexBox> = (args) => {
+  const ref = useRef<HTMLUListElement>(null);
+
+  return (
+    <FlexBox
+      as="ul"
+      ref={ref}
+      sx={{
+        bgColor: 'primary',
+        width: '10rem',
+        height: '5rem',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      {...args}
+    >
+      <FlexBox>1</FlexBox>
+      <FlexBox sx={{ alignSelf: 'flex-start' }}>2</FlexBox>
+      <FlexBox>3</FlexBox>
+    </FlexBox>
+  );
+};
