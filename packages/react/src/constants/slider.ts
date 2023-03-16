7335f1e8a34e2a40c34c16f27442361e8cf6c4d7
Hemudi <ksum1205@naver.com>
2023-03-17 02:48:38 +0900

    :sparkles: add story code of slider

---
+++ packages/react/src/constants/slider.ts
@@ -0,0 +1,68 @@
+export const noticeList = [
+  {
+    id: 1,
+    title: '스터디 가입전 해당 공지사항을 숙지해주세요.',
+    writer: '파크',
+    date: 1669750385300,
+    view: 101,
+  },
+  {
+    id: 2,
+    title: '스터디 전체 공지 드립니다.',
+    writer: '도니',
+    date: 1662476400000,
+    view: 110,
+  },
+  {
+    id: 3,
+    title: '스터디 전체 공지 드립니다.',
+    writer: '햄디',
+    date: 1662476400000,
+    view: 9,
+  },
+  {
+    id: 4,
+    title: '스터디 전체 공지 드립니다.',
+    writer: '제이미',
+    date: 1668750300553,
+    view: 9,
+  },
+];
+
+export const myStudies = [
+  {
+    id: '999',
+    title: '출석 스터디',
+    description: '매일 오전 출석하고 글로 체크아웃 하는 스터디',
+  },
+  {
+    id: '998',
+    title: '리액트 스터디',
+    description: '리액트 공식문서로 스터디',
+  },
+  {
+    id: '997',
+    title: '자바스크립트 스터디',
+    description: '딥다이브 뿌시기',
+  },
+  {
+    id: '996',
+    title: '면접 스터디',
+    description: '떨지말아요 면접',
+  },
+  {
+    id: '995',
+    title: '운동 스터디',
+    description: '하루 만보 걷기 프로젝트',
+  },
+  {
+    id: '994',
+    title: '손캠 스터디',
+    description: '놀고있는거 다 알아 +_+',
+  },
+  {
+    id: '993',
+    title: '요리 스터디',
+    description: '탈출해보자 요리고자!',
+  },
+];
