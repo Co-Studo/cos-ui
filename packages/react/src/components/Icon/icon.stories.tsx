0ee000f367b6ebb5e18d2ea79f9573270c5d9934
Hemudi <ksum1205@naver.com>
2023-03-17 02:14:18 +0900

    :sparkles: add playground stories code of icon

--- packages/react/src/components/Icon/icon.stories.tsx
+++ packages/react/src/components/Icon/icon.stories.tsx
@@ -1,6 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import { FlexBox } from '@components/FlexBox';
 import Icon from '@components/Icon';
+import Text from '@components/Text';
 import { lightTheme } from '@styles/theme';
 
 export default {
@@ -25,4 +27,27 @@ export default {
 
 const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
 
-export const Default = Template.bind({});
+export const Default = () => (
+  <div>
+    <Text variant="sectionTitle">Icon Name</Text>
+    <FlexBox sx={{ gap: '1rem', mt: 1, mb: 2 }}>
+      <Icon iconName="arrow" />
+      <Icon iconName="bell" />
+      <Icon iconName="crown" />
+      <Icon iconName="flag" />
+      <Icon iconName="lock" />
+      <Icon iconName="sort" />
+      <Icon iconName="star" />
+    </FlexBox>
+    <Text variant="sectionTitle">Icon Size</Text>
+    <FlexBox sx={{ gap: '1rem', mt: 1, mb: 2 }}>
+      <Icon iconName="star" size="xSmall" />
+      <Icon iconName="star" size="small" color="neutral_1" />
+      <Icon iconName="star" size="medium" color="neutral_1" />
+      <Icon iconName="star" size="large" color="neutral_2" />
+      <Icon iconName="star" size="xLarge" color="neutral_2" />
+    </FlexBox>
+  </div>
+);
+
+export const Custom = Template.bind({});
