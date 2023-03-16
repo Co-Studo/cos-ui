ab6938dbb8d590507bcf2f79981a947d286760db
Hemudi <ksum1205@naver.com>
2023-03-17 02:15:57 +0900

    :sparkles: add playground stories code of loadingSpinner

--- packages/react/src/components/LoadingSpinner/loadingSpinner.stories.tsx
+++ packages/react/src/components/LoadingSpinner/loadingSpinner.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import LoadingSpinner from '@components/LoadingSpinner';
+import Text from '@components/Text';
 
 export default {
   title: 'React/LoadingSpinner',
@@ -11,4 +12,21 @@ const Template: ComponentStory<typeof LoadingSpinner> = (args) => (
   <LoadingSpinner {...args} />
 );
 
-export const Default = Template.bind({});
+export const Default = () => (
+  <>
+    <div css={{ position: 'relative', width: '20rem', height: '15rem' }}>
+      <Text variant="sectionTitle">Small</Text>
+      <LoadingSpinner size="small" />
+    </div>
+    <div css={{ position: 'relative', width: '20rem', height: '15rem' }}>
+      <Text variant="sectionTitle">Medium</Text>
+      <LoadingSpinner />
+    </div>
+    <div css={{ position: 'relative', width: '20rem', height: '15rem' }}>
+      <Text variant="sectionTitle">Large</Text>
+      <LoadingSpinner size="large" />
+    </div>
+  </>
+);
+
+export const Custom = Template.bind({});
