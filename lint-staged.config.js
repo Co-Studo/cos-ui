7edfaa0359850660c79f2e93faa6c519d2f379ce
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-05-03 15:58:42 +0900

    :ambulance: except noEmit option & remove exclude stories

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': 'eslint --fix',
   '*.{js,jsx,ts,tsx,json}': 'prettier --write',
-  'packages/react/**/*.+(ts|tsx)': () =>
-    'tsc -p packages/react/tsconfig.json --noEmit',
-  'packages/playground-react/**/*.+(ts|tsx)': () =>
-    'tsc -p packages/playground-react/tsconfig.json --noEmit',
+  'packages/react/**/*.+(ts|tsx)': () => 'tsc -p packages/react/tsconfig.json',
   'packages/primitives/**/*.+(ts|tsx)': () =>
-    'tsc -p packages/primitives/tsconfig.json --noEmit',
+    'tsc -p packages/primitives/tsconfig.json',
 };
