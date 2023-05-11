ea86cce078099cd05e02a63e54b966fb5ce75246
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-05-11 19:54:46 +0900

    :fire: remove absolute path in index.ts

--- packages/primitives/src/index.ts
+++ packages/primitives/src/index.ts
@@ -1,13 +1,13 @@
 export {
   default as ClickAway,
   useClickAwayContext,
-} from '@components/ClickAway';
-export { default as Dropdown } from '@components/Dropdown';
-export { default as Form } from '@components/Form';
-export { default as Radio } from '@components/Radio';
-export { default as Slider } from '@components/Slider';
-export { default as Tab } from '@components/Tab';
-export { default as TextInput } from '@components/TextInput';
-export { default as TextArea } from '@components/TextArea';
-export { ColumnTable, RowTable, BasicTable } from '@components/Table';
-export * from '@components/IntersectListener';
+} from './components/ClickAway';
+export { default as Dropdown } from './components/Dropdown';
+export { default as Form } from './components/Form';
+export { default as Radio } from './components/Radio';
+export { default as Slider } from './components/Slider';
+export { default as Tab } from './components/Tab';
+export { default as TextInput } from './components/TextInput';
+export { default as TextArea } from './components/TextArea';
+export { ColumnTable, RowTable, BasicTable } from './components/Table';
+export * from './components/IntersectListener';
