5fcb29a685b99cb349d2163a4720dfae72e13009
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-05-19 15:43:33 +0900

    :wrench: add babel plugins for svg, css prop

--- packages/primitives/vite.config.ts
+++ packages/primitives/vite.config.ts
@@ -1,8 +1,14 @@
 import { resolve } from 'path';
 
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [
+    react({
+      babel: { plugins: ['babel-plugin-styled-components'] },
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,8 +1,40 @@
 import { resolve } from 'path';
 
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          'babel-plugin-styled-components',
+          [
+            'inline-react-svg',
+            {
+              svgo: {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                  {
+                    name: 'removeAttrs',
+                    params: { attrs: '(data-name)' },
+                  },
+                  'cleanupIDs',
+                ],
+              },
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
